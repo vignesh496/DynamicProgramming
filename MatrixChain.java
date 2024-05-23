79ff441b4f1bc9cc7ad4cda4b0663a772065a4b9
Vignesh G <115526040+vignesh496@users.noreply.github.com>
2024-05-23 13:36:02 +0530

    Update MatrixChain.java

--- MatrixChain.java
+++ MatrixChain.java
@@ -2,56 +2,42 @@ import java.util.*;
 
 class MatrixChain {
     public static void main(String[] args) {
-        int[] p = {4, 10, 3, 12, 20, 7};
+        int[] p = {5, 4, 3 , 2, 1, 6};
         matrixChain(p);
     }
-    static void matrixChain(int[] p)    {
-        int n = p.length - 1;
+    public static void matrixChain(int[] p) {
+        int n = p.length;
         int[][] M = new int[n][n];
-        int[][] s = new int[n][n]; 
+        int[][] s = new int[n][n];
 
         for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                M[i][j] = Integer.MAX_VALUE;
+            }
             M[i][i] = 0;
         }
 
-        for (int l = 2; l <= n; l++) {
-            for (int i = 0; i <= n - l; i++) {
-                int j = i + l - 1;
-                M[i][j] = Integer.MAX_VALUE;
+        for (int l = 2; l < n; l++) {
+            for (int i = 1; i < n-l+1; i++) {
+                int j = l + i - 1;
                 for (int k = i; k < j; k++) {
-                    int q = M[i][k] + M[k + 1][j] + p[i] * p[k + 1] * p[j + 1];
-                    if (q < M[i][j]) {
-                        M[i][j] = q;
+                    int a = M[i][k] + M[k+1][j] + (p[i-1] * p[k] * p[j]);
+                    if (a <= M[i][j])    {
+                        M[i][j] = a;
                         s[i][j] = k;
                     }
                 }
             }
         }
 
-        System.out.println("Minimum number of multiplications is " + M[0][n-1]);
-
-        System.out.println("M matrix:");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i <= j) {
-                    System.out.print(M[i][j] + "\t");
-                } else {
-                    System.out.print(" \t");
-                }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                System.out.print(M[i][j] + " ");
             }
             System.out.println();
         }
 
-        System.out.println("s matrix:");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i < j) {
-                    System.out.print(s[i][j] + "\t");
-                } else {
-                    System.out.print(" \t");
-                }
-            }
-            System.out.println();
-        }
+        System.out.println("Minimum no. of multiplications : " + M[1][n-1]);
+
     }
 }
